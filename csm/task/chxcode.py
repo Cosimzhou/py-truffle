d8f15c2ed9832dcf5aea868d10dd410ba407dfc3
zhouzhichao <zhouzhichao@tigerknows.com>
2016-04-21 13:40:49 +0800

    codewc: upgrade display mode

--- csm/task/chxcode.py
+++ csm/task/chxcode.py
@@ -1,19 +1,33 @@
 #coding: UTF-8
 
-started, ended = False, False
-of = open('/Users/zhouzhichao/workspace/tigerknows-mapcore/x86_64-macosx-clang-debug.xcode.build/tkengine-1.xcodeproj','w')
+
+
+# started, ended = False, False
+# of = open('/Users/zhouzhichao/workspace/tigerknows-mapcore/x86_64-macosx-clang-debug.xcode.build/tkengine-1.xcodeproj','w')
+
 with open('/Users/zhouzhichao/workspace/tigerknows-mapcore/x86_64-macosx-clang-debug.xcode.build/tkengine.xcodeproj','r') as f:
+    probj = {}
     for l in f.readlines():
-        if started and not ended:
-            
-            
-            continue
-        elif "/* End PBXGroup section */" in l:
-            ended = True
-            
+        l = l.strip('\n')
+        for c in l:
             pass
-            
-        elif "/* Begin PBXGroup section */" in l:
-            started = True
-        of.write(l)
+#         
+#         if started and not ended:
+#             
+#             
+#             continue
+#         elif "/* End PBXGroup section */" in l:
+#             ended = True
+#             
+#             pass
+#             
+#         elif "/* Begin PBXGroup section */" in l:
+#             started = True
+#         of.write(l)
         
+exec '''
+b={ 'asd' """b """: 4}
+'''
+# b = _
+
+print b
--- csm/task/codewc.py
+++ csm/task/codewc.py
@@ -12,11 +12,11 @@ def fileLineNum(file_name):
         with open(file_name, 'r') as f:
             cnt, length = 0, 0
             for l in f.readlines():
-                l = l.strip(' \t\n')
+                l = l.strip(' \t\r\n')
                 if l: 
                     cnt += 1
                     length += len(l)
-            return cnt,length
+            return [cnt,length]
     return None
 """        
 def countWorkspace(workspace_path):
@@ -62,8 +62,8 @@ def countWorkspace(workspace_path):
 #             else:
 #                 sys.stderr.write('error:%s'+l.strip('\n'))
     return 0#wcsum
-"""
-def countWorkspace(workspace_path):
+#" ""
+def countWorkspace_old(workspace_path):
     buffers,stkDir = [], []
     slwp = len(workspace_path)
     for root, _, filelist in os.walk(workspace_path):
@@ -129,10 +129,72 @@ def countWorkspace(workspace_path):
     
     return buffers[0][1]#wcsum
 #"""
+
+def countWorkspace(workspace_path):
+    def getCDInfo(buf, root):
+        md, v = buf, None
+        for d in root.split('/'):
+            if d in md:
+                v = md[d]
+            else:
+                v = md[d] = [0, 0, {}]
+            md = v[2]
+        return v
+    def updateCDInfos(cdinfos):
+        if len(cdinfos) == 3:
+            l,b = 0,0
+            useless=[]
+            for k,v in cdinfos[2].items():
+                v[0:2] = updateCDInfos(v)
+                if v[0] == 0:
+                    useless.append(k)
+                else:
+                    l +=v[0]
+                    b +=v[1]
+            for k in useless:
+                del cdinfos[2][k]
+            return l,b
+        else:
+            return cdinfos
+    
+    def outputResult(buff, depth=0, before=None):
+        if before is None: before = [False]*100
+        if len(before) <= depth: before.append(False)
+        dirs, n = sorted(buff.keys()), 0
+        for d in dirs:
+            cdinfo = buff[d]
+            n += 1
+            if n <len(dirs):
+                before[depth], anchor =True, '┣'
+            else:
+                before[depth], anchor =False, '┗'
+                
+            prefix = ''
+            for i in xrange(depth-1):
+                prefix += '┃' if before[i+1] else '  '
+            if depth: prefix+=anchor
+            print '%s%s%s:  %d, %d'%(prefix, ' ' if depth else'', d, cdinfo[0], cdinfo[1])
+            if len(cdinfo) == 3:
+                outputResult(cdinfo[2], depth+1, before)
+    
+    mbuffer = {}
+    slwp = len(workspace_path)
+    for root, _, filelist in os.walk(workspace_path):
+        root = '.'+root[slwp:]
+        cdinfo = getCDInfo(mbuffer, root)
+        for f in filelist:
+            cnt = fileLineNum('/'.join((workspace_path,root,f)))
+            if cnt is not None:
+                cdinfo[2][f] = cnt 
+                
+    mbuffer['.'][0:2] = updateCDInfos(mbuffer['.'])         
+    outputResult(mbuffer)
+    
+    return mbuffer['.'][0]
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='''统计代码行数''')
     parser.add_argument('directory', metavar='N', type=str, nargs='+', help='诗句的内容，每句以空格分隔')
-#     parser.add_argument('--sourceCode', '-s', default='GBK', help='输入文件的编码方式，默认为“GBK”')
     args = parser.parse_args()
     
     for direct in args.directory:
